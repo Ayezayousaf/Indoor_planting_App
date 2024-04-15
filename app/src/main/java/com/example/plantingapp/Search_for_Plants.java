6d81679a972dc70f31d9f462a330abbdfba57102
IZZU <ayezarajpoot2002@gmail.com>
2024-04-15 21:15:26 +0500

    Git Data new Added

--- app/src/main/java/com/example/plantingapp/Search_for_Plants.java
+++ app/src/main/java/com/example/plantingapp/Search_for_Plants.java
@@ -17,7 +17,6 @@ public class Search_for_Plants extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         searchView=findViewById(R.id.searchView);
-        listView=findViewById(R.id.listView);
         searchView.clearFocus();
 
     }
